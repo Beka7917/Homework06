7adc94cdfe8a4e05d11ebc1e475cdf7b8d7ada84
bekakad0303 <bekturkadyrkulov@icloud.com>
2023-02-22 15:19:34 +0600

    Homework6

--- Homework06/main.swift
+++ Homework06/main.swift
@@ -7,5 +7,27 @@
 
 import Foundation
 
-print("Hello, World!")
+
+// первое задание
+print("Введите слово")
+var readline  = readLine()!
+
+var word:(String) -> Int
+word = { (pl:String) -> Int in return pl.count
+
+}
+
+print(readline.count)
+// второе задание
+var dollar:(Float,Float) -> Float
+dollar = {$0 * $1}
+
+var result = dollar(250, 87.42)
+
+print("\(result) cом ")
+
+// третье задание
+var array1:[String] = ["Bektur","Tala","Radik"]
+array1.sort{$0 > $1}
+print(array1)
 
